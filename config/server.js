23ab5cb8dba2be4cfa8f840fbcf90f005cedaa56
Mike McCready <mikem@inspero.net>
2016-12-05 13:45:27 -0800

    add bodyParser middleware, create get and post api routes for workouts => route to workout controller

--- config/server.js
+++ config/server.js
@@ -1,15 +1,20 @@
 //server.js
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const workoutController = require('./controllers/workout_controller');
+console.log(workoutController);
+
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  response.send('serving from /')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/api/workouts', workoutController.getWorkouts);
+app.post('/api/workouts', workoutController.createWorkout);
 
 app.listen(PORT, (err) => {
-  if (err) {
-    return console.log('Error: ', err);
-  }
+  if (err) return console.log('Error: ', err);
   console.log('app is listening port 3000');
 });
