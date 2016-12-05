69e73d0005f3429dfb8325adfa072cabf12ce828
Mike McCready <mikem@inspero.net>
2016-12-05 11:35:42 -0800

    refactor db.js to use pg.pool

--- config/db.js
+++ config/db.js
@@ -1,8 +1,17 @@
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/trainr';
 
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-  'CREATE TABLE workouts(id SERIAL PRIMARY KEY, weight INT, movement TEXT)');
-query.on('end', () => { client.end(); });
+const pool = new pg.Pool(connectionString);
+
+pool.connect((err, client, done) => {
+  if(err) return console.error('error fetching client from pool', err);
+  client.query('DROP TABLE workouts; CREATE TABLE workouts(id SERIAL PRIMARY KEY, weight INT, movement TEXT)', (err, result) => {
+    done();
+    if(err) return console.error('error running query', err);
+    console.log(result);
+  });
+});
+
+pool.on('error', function (err, client) {
+  console.error('idle client error', err.message, err.stack)
+})
