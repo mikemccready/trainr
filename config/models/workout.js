13d0437534b688d7d7404c4ad45848149f0bbc18
Mike McCready <mikem@inspero.net>
2016-12-07 00:55:12 -0800

    remove workout_data from workouts

--- config/models/workout.js
+++ config/models/workout.js
@@ -6,11 +6,13 @@ const config = {
   max: 10,
   idleTimeoutMillis: 30000,
 };
+
 const pool = new pg.Pool(config);
 
 pool.connect((err, client, done) => {
   if(err) return console.error('error fetching client from pool', err);
-  client.query('DROP TABLE IF EXISTS workouts; CREATE TABLE workouts(id SERIAL PRIMARY KEY, workout_data JSON, date timestamptz)', (err, result) => {
+  client.query('DROP TABLE IF EXISTS workouts; CREATE TABLE workouts(id serial primary key, date TIMESTAMPTZ)',
+  (err, result) => {
     done();
     if(err) return console.error('error running query', err);
     console.log(result);
@@ -19,4 +21,4 @@ pool.connect((err, client, done) => {
 
 pool.on('error', function (err, client) {
   console.error('idle client error', err.message, err.stack)
-})
+});
