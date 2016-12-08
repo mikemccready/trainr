e20101b14d8910b2a785b0af1a5ecfa7bf9dfbee
Mike McCready <mikem@inspero.net>
2016-12-08 13:38:11 -0800

    passing password to hash function before saving user

--- config/controllers/user_controller.js
+++ config/controllers/user_controller.js
@@ -2,32 +2,45 @@ const promise = require('bluebird');
 const options = {
   promiseLib: promise
 };
-
 const pgp = require('pg-promise')(options);
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/trainr';;
 const db = pgp(connectionString);
 
+const bcrypt = require('bcrypt-nodejs');
+
 function signupUser(req, res) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) return res.status(422).send({error: 'Please provide email and password'});
   // check if email already exists
   db.any(`SELECT * FROM users WHERE email = '${email}';`)
     .then(userData => {
       if (userData.length > 0) {
         return res.status(422).send({ error: 'Email already exists' });
+      } else {
+        // no user exists, create new user
+        hashPassword(req, res, email, password);
       }
-      // no user exists, create new user
-      createUser(req, res, email, password);
     })
     .catch(err => {
       return res.status(500).send({ error: err }).end();
     });
 }
 
-function createUser(req, res, email, password) {
+function hashPassword(req, res, email, password) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return err;
+    bcrypt.hash(password, salt, null, (err, hashedpassword) => {
+      createUser(req, res, email, hashedpassword);
+    });
+  });
+}
+
+function createUser(req, res, email, hashedPassword) {
   db.none(
     `INSERT INTO users(email, password, created_on)
-     VALUES('${email}', '${password}', now())`
+     VALUES('${email}', '${hashedPassword}', now())`
   ).then(data => {
       res.status(200).json({ status: 'success', message: 'User created'});
       return res.end();
@@ -47,6 +60,7 @@ function getUsers(req, res) {
       return res.status(500).send('Error getting users').end();
     })
 }
+
 
 module.exports = {
   signupUser,
