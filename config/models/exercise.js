a919c033f5d83111651b779cfd7d72231928ddea
Mike McCready <mikem@inspero.net>
2016-12-08 00:37:01 -0800

    refactoring exercise model queries

--- config/models/exercise.js
+++ config/models/exercise.js
@@ -11,12 +11,31 @@ const pool = new pg.Pool(config);
 
 pool.connect((err, client, done) => {
   if(err) return console.error('error fetching client from pool', err);
-  client.query('DROP TABLE IF EXISTS exercises; CREATE TABLE exercises(id serial primary key, movement VARCHAR(255), weight INT, workout_id INT REFERENCES workouts (id))',
-  (err, result) => {
-    done();
-    if(err) return console.error('error running query', err);
-    console.log(result);
-  });
+  client.query('DROP TABLE IF EXISTS exercise;',
+    (err, result) => {
+      done();
+      if(err) return console.error('error running query', err);
+      console.log('Table Dropped');
+    }
+  );
+  client.query(
+    `CREATE TABLE exercise(
+      exercise_id serial PRIMARY KEY,
+      movement VARCHAR(50) NOT NULL,
+      weight integer,
+      notes VARCHAR(1000),
+      created_on TIMESTAMPTZ NOT NULL,
+      workout_id integer NOT NULL,
+      CONSTRAINT exercise_workout_id_fkey FOREIGN KEY (workout_id)
+        REFERENCES workout (workout_id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+    );`,
+    (err, result) => {
+      done();
+      if(err) return console.error('error running query', err);
+      console.log('Exercise table created');
+    }
+  );
 });
 
 pool.on('error', function (err, client) {
