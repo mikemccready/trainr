fc9170d3ef955f4aea5bf06961137b118918eb13
Mike McCready <mikem@inspero.net>
2016-12-10 00:57:47 -0800

    import routes from routes.js, creat store with root reducer, wrap router in provider

--- client/index.js
+++ client/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, browserHistory } from 'react-router'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import Main from './containers/Main';
-import Signup from './components/Signup';
-import Signin from './components/Signin';
-import Welcome from './components/Welcome';
-import Start from './components/Start';
-import Trainr from './components/Trainr';
-import Progress from './components/Progress';
+import routes from './routes';
+import reducers from './reducers';
 
+const store = createStore(reducers);
 const app = document.getElementsByClassName('app')[0];
 
 render((
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={Welcome} />
-      <Route path="signup" component={Signup} />
-      <Route path="signin" component={Signin} />
-      <Route path="start" component={Start} />
-      <Route path="trainr" component={Trainr} />
-      <Route path="progress" component={Progress} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
 ), app);
