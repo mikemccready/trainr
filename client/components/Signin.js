f2f2a8f7832c338e34fae24b71027f7ba2cfd37a
Mike McCready <mikem@inspero.net>
2016-12-09 18:57:48 -0800

    init signin, signup, and welcome components

---
+++ client/components/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'react-router';
+
+export default function Signin(props) {
+  return (
+      <div className="signin-page">
+        <h3>Sign in</h3>
+      </div>
+  )
+}
