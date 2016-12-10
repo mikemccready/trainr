c0954238a8ed9c1c85794ad1f45e8911dc07be07
Mike McCready <mikem@inspero.net>
2016-12-09 18:45:33 -0800

    import signup, signin and welcom components

--- client/index.js
+++ client/index.js
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import Main from './containers/Main';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Welcome from './components/Welcome';
 import Start from './components/Start';
 import Trainr from './components/Trainr';
 import Progress from './components/Progress';
@@ -12,7 +15,10 @@ const app = document.getElementsByClassName('app')[0];
 render((
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
-      <IndexRoute component={Start} />
+      <IndexRoute component={Welcome} />
+      <Route path="signup" component={Signup} />
+      <Route path="signin" component={Signin} />
+      <Route path="start" component={Start} />
       <Route path="trainr" component={Trainr} />
       <Route path="progress" component={Progress} />
     </Route>
