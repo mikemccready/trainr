a234cee8bc4ea7e22f07359f9e736c003604d2cf
Mike McCready <michaeljmccready@gmail.com>
2016-12-14 10:59:44 -0800

    add signout function and link

--- client/containers/Main.js
+++ client/containers/Main.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
+
 
 import * as actionCreators from '../actions/action_creators_user';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.signout = this.signout.bind(this);
+  }
+
+  signout() {
+    localStorage.removeItem('token');
+    this.props.signout();
+    browserHistory.push('/');
+  }
+
   render() {
+    let signoutLink;
+    if (this.props.user.authenticated) {
+      signoutLink = <h3 onClick={this.signout}>Sign out</h3>;
+    }
+
     return (
       <div className="main-container" onClick={this.handleClicks}>
         <h1>trainr</h1>
+        { signoutLink }
         { React.cloneElement(this.props.children, this.props) }
       </div>
     );
