cd6abc3f3272016805c9f6c39aaed0cc906382c7
Mike McCready <michaeljmccready@gmail.com>
2016-12-14 00:16:27 -0800

    check if token exists => dispatch to authenticate with user_id

--- client/index.js
+++ client/index.js
@@ -5,8 +5,19 @@ import { Provider } from 'react-redux';
 
 import store, { history } from './store';
 import routes from './routes';
+import { decode } from './services/token';
 
 const app = document.getElementsByClassName('app')[0];
+
+// check if token exists
+const token = localStorage.getItem('token');
+if (token) {
+  const user_id = decode(token);
+  store.dispatch({
+    type: 'AUTHENTICATE',
+    user_id
+  })
+}
 
 render((
   <Provider store={store}>
