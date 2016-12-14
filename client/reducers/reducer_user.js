052e72daadcfe632180b8a4e2cea14dcda0c2dca
Mike McCready <michaeljmccready@gmail.com>
2016-12-14 11:00:19 -0800

    add case for signout in user reducer

--- client/reducers/reducer_user.js
+++ client/reducers/reducer_user.js
@@ -1,11 +1,18 @@
 export default (state = {}, action) => {
+  let newState;
   switch (action.type) {
     case 'AUTHENTICATE':
-    const newState = Object.assign({}, state, {
-      authenticated: true,
-      user_id: action.user_id
-    });
-    return newState;
+      newState = Object.assign({}, state, {
+        authenticated: true,
+        user_id: action.user_id
+      });
+      return newState;
+    case 'SIGNOUT':
+      newState = {
+        authenticated: false,
+        user_id: null
+      }
+      return newState;
   }
   return state;
 }
