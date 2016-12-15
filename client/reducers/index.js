9da0583abcccb466372881a0b6e69b34b2d37e7e
Mike McCready <michaeljmccready@gmail.com>
2016-12-15 13:55:25 -0800

    import exercise reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,11 +3,13 @@ import { routerReducer } from 'react-router-redux';
 
 import user from './reducer_user';
 import workouts from './reducer_workouts';
+import exercises from './reducer_exercises';
 import currentWorkout from './reducer_current_workout.js';
 
 const rootReducer = combineReducers({
   user,
   workouts,
+  exercises,
   currentWorkout,
   routing: routerReducer
 });
