63a22ac3755976a460b11b1794f351d25486c370
Mike McCready <michaeljmccready@gmail.com>
2016-12-15 10:17:12 -0800

    add currentWorkout reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,10 +3,12 @@ import { routerReducer } from 'react-router-redux';
 
 import user from './reducer_user';
 import workouts from './reducer_workouts';
+import currentWorkout from './reducer_current_workout.js';
 
 const rootReducer = combineReducers({
   user,
   workouts,
+  currentWorkout,
   routing: routerReducer
 });
 
