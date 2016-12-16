5dfc3ecb951410c9583ff7124d81933d84cd6357
Mike McCready <michaeljmccready@gmail.com>
2016-12-15 20:34:47 -0800

    move getWorkout async to workout action creators

--- client/actions/action_creators_workouts.js
+++ client/actions/action_creators_workouts.js
@@ -1,7 +1,29 @@
-export function storeWorkouts(workouts) {
+function storeWorkouts(workouts) {
   return {
     type: 'STORE_WORKOUTS',
     workouts
+  }
+}
+
+export function getWorkoutsReq(user_id) {
+  const userId = user_id;
+  return (dispatch) => {
+    const token = localStorage.getItem('token');
+    fetch(`http://localhost:3000/api/users/${userId}/workouts`, {
+      method: 'GET',
+      headers: new Headers({
+        'authorization': token
+      })
+    })
+      .then(response => {
+        if (response.status !== 200) return console.log('error', response.status);
+        response.json().then(data => {
+          dispatch(storeWorkouts(data));
+        })
+      })
+      .catch(err => {
+        console.log('error', err);
+      });
   }
 }
 
