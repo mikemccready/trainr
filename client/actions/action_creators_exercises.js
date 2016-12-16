38814b60267359dc74bdcc10e033af52af41479d
Mike McCready <michaeljmccready@gmail.com>
2016-12-15 20:33:52 -0800

    move getExercises async to exercise action creators

--- client/actions/action_creators_exercises.js
+++ client/actions/action_creators_exercises.js
@@ -4,3 +4,25 @@ export function storeExercises(exercises) {
     exercises
   }
 }
+
+export function getExercisesReq(user_id) {
+  return (dispatch) => {
+    const token = localStorage.getItem('token');
+    const userId = user_id;
+    fetch(`http://localhost:3000/api/users/${userId}/exercises`, {
+      method: 'GET',
+      headers: new Headers({
+        'authorization': token
+      })
+    })
+      .then(response => {
+        if (response.status !== 200) return console.log('error', response.status);
+        response.json().then(data => {
+          dispatch(storeExercises(data));
+        })
+      })
+      .catch(err => {
+        console.log('error', err);
+      })
+  }
+}
