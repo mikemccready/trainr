c4590e30fa5beb32c4a61ac9cb23376d4d0e3792
Mike McCready <michaeljmccready@gmail.com>
2016-12-15 20:35:49 -0800

    move getExercises and getWorkout async functions to action creator

--- client/components/Progress.js
+++ client/components/Progress.js
@@ -4,57 +4,8 @@ import { browserHistory } from 'react-router';
 export default class Progress extends React.Component {
   constructor(props) {
     super(props);
-    this.getWorkoutsReq = this.getWorkoutsReq.bind(this);
-    this.getExercisesReq = this.getExercisesReq.bind(this);
     this.addWorkoutReq = this.addWorkoutReq.bind(this);
     this.goToWorkoutSummary = this.goToWorkoutSummary.bind(this);
-  }
-
-  componentDidMount() {
-    this.getWorkoutsReq();
-    this.getExercisesReq();
-  }
-
-  getWorkoutsReq() {
-    const that = this;
-    const token = localStorage.getItem('token');
-    const user_id = this.props.user.user_id;
-    fetch(`http://localhost:3000/api/users/${user_id}/workouts`, {
-      method: 'GET',
-      headers: new Headers({
-        'authorization': token
-      })
-    })
-      .then(response => {
-        if (response.status !== 200) return console.log('error', response.status);
-        response.json().then(data => {
-          that.props.storeWorkouts(data);
-        })
-      })
-      .catch(err => {
-        console.log('error', err);
-      })
-  }
-
-  getExercisesReq() {
-    const that = this;
-    const token = localStorage.getItem('token');
-    const user_id = this.props.user.user_id;
-    fetch(`http://localhost:3000/api/users/${user_id}/exercises`, {
-      method: 'GET',
-      headers: new Headers({
-        'authorization': token
-      })
-    })
-      .then(response => {
-        if (response.status !== 200) return console.log('error', response.status);
-        response.json().then(data => {
-          that.props.storeExercises(data);
-        })
-      })
-      .catch(err => {
-        console.log('error', err);
-      })
   }
 
   addWorkoutReq() {
