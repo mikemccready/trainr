48ddcdad31aeb5abe8bf56a2dd308ab84dc9403f
Mike McCready <michaeljmccready@gmail.com>
2016-12-16 21:41:16 -0800

    create function to save exercise and dispatch data on success

--- client/actions/action_creators_exercises.js
+++ client/actions/action_creators_exercises.js
@@ -1,7 +1,14 @@
-export function storeExercises(exercises) {
+function storeExercises(exercises) {
   return {
     type: 'STORE_EXERCISES',
     exercises
+  }
+}
+
+function addExercise(exercise) {
+  return {
+    type: 'ADD_EXERCISE',
+    exercise
   }
 }
 
@@ -19,6 +26,31 @@ export function getExercisesReq(user_id) {
         if (response.status !== 200) return console.log('error', response.status);
         response.json().then(data => {
           dispatch(storeExercises(data));
+        })
+      })
+      .catch(err => {
+        console.log('error', err);
+      })
+  }
+}
+
+export function saveExerciseReq(exerciseData, workout_id, user_id) {
+  return (dispatch) => {
+    const token = localStorage.getItem('token');
+    const reqBody = Object.assign({}, exerciseData, { user_id }, { workout_id });
+    console.log(reqBody)
+    fetch(`http://localhost:3000/api/users/${user_id}/exercises`, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'authorization': token
+      }),
+      body: JSON.stringify(reqBody)
+    })
+      .then(response => {
+        if (response.status !== 200) return console.log('error', response.status);
+        response.json().then(data => {
+          dispatch(addExercise(data.data));
         })
       })
       .catch(err => {
