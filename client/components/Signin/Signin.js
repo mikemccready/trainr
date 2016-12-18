a20cc5d361f348501e185cb6384b8ab8f1635f30
Mike McCready <michaeljmccready@gmail.com>
2016-12-18 12:50:13 -0800

    move components to seperate directories, init scss module

---
+++ client/components/Signin/Signin.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Link, browserHistory } from 'react-router';
+
+import { decode } from '../../services/token';
+import styles from './signin.scss';
+
+export default class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMsg: '',
+      email: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.showError = this.showError.bind(this);
+  }
+
+  handleChange(e) {
+    const field = e.target.id;
+    const value = e.target.value;
+    switch (field) {
+      case 'signin-email':
+        this.setState({email: value});
+        break;
+      case 'signin-password':
+        this.setState({password: value});
+        break;
+      default:
+        break;
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const that = this;
+    const formData = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    this.setState({errorMsg: ''})
+
+    fetch('http://localhost:3000/api/users/signin', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify(formData)
+    })
+      .then(response => {
+        if (response.status !== 200) return that.showError();
+        response.json().then(data => {
+          const user_id = decode(data.token);
+          localStorage.setItem('token', data.token);
+          that.props.authenticate(user_id);
+          browserHistory.push('/progress');
+        })
+      })
+      .catch(err => {
+        console.log('error', err);
+      })
+  }
+
+  showError() {
+    return this.setState({errorMsg: 'Invalid email or password'});
+  }
+
+  render() {
+    return (
+      <div className={styles.signin}>
+        <h1>trainr</h1>
+        <div className={styles['signin-form-container']}>
+          <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+            <h5>{this.state.errorMsg}</h5>
+            <section>
+              <p>
+                <label htmlFor="signin-email">
+                  <input
+                    type="email"
+                    id="signin-email"
+                    placeholder="me@trainr.com"
+                    value={this.state.email} />
+                </label>
+              </p>
+              <p>
+                <label htmlFor="signin-password">
+                  <input
+                    type="password"
+                    id="signin-password"
+                    placeholder="password"
+                    value={this.state.password} />
+                </label>
+              </p>
+            </section>
+            <section>
+              <p>
+                <button>Sign in</button>
+              </p>
+            </section>
+          </form>
+          <Link to="/signup">New to trainr? Sign up</Link>
+        </div>
+      </div>
+    )
+  }
+}
