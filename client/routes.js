32021794e98fdf1938ade08a7ceebaa4262720c3
Mike McCready <michaeljmccready@gmail.com>
2016-12-17 21:51:24 -0800

    update component paths

--- client/routes.js
+++ client/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import Main from './containers/Main';
+import Main from './containers/Main/Main';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Welcome from './components/Welcome';
-import WorkoutSummary from './components/WorkoutSummary';
-import Trainr from './components/Trainr';
-import Progress from './components/Progress';
+import WorkoutSummary from './components/WorkoutSummary/WorkoutSummary';
+import Trainr from './components/Trainr/Trainr';
+import Progress from './components/Progress/Progress';
 
 export default (
   <Route path="/" component={Main}>
